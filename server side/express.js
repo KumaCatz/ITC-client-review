d84548835ed2079be0cccf15b69663bc42720771
Liora Boccara <lioraboccara@gmail.com>
2023-09-27 18:11:13 +0200

    adding server

---
+++ server side/express.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors({
+    origin: '*'
+}));
+app.use(express.json());
+
+//error handling 
+app.use((err, req, res, next) => {
+    console.log(err);
+
+    try {
+        const [statusCode, msg] = err;
+
+        res.status(statusCode).send({
+            error: true,
+            message: msg
+        })
+    } catch (error) {
+        res.status(500).send({
+            error: true,
+            message: err.message
+        })
+    }
+})
+
+app.listen(2500, () => {
+    console.log('express is listening on port: 2500');
+})
